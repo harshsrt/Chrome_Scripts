eef4d65087b88f208fcd6d1bef208dd6529ea4e2
harshsrt <harsh.harsh17@gmail.com>
2017-11-03 20:32:48 +0530

    F12 press

--- Firstproject/src/Chrome/New.java
+++ Firstproject/src/Chrome/New.java
@@ -1,10 +1,27 @@
 package Chrome;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
 public class New {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-
-	}
+		System.setProperty("webdriver.chrome.driver","C:\\Selenium\\chromedriver_win32\\chromedriver.exe");
+   		WebDriver driver = new ChromeDriver();
+   		driver.get("https://www.msn.com/en-us/");
+   		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+   		Robot robot = new Robot();
+   		robot.keyPress(KeyEvent.VK_F12);
+   		robot.keyRelease(KeyEvent.VK_F12);	
+   	 }
 
 }
